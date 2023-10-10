347e827ff6fecefa1fb1036649e537d7149ab2d1
Acsa <acsa.silveiras@gmail.com>
2023-10-10 10:14:48 -0300

    Acsa 10/10

---
+++ resources/views/sistema/layout/layoutErro.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite(['resources/js/app.js', 'resources/css/projeto.css'])
+</head>
+</head>
+<body>
+    @endcomponent
+    <main role="main" class="py-4">
+        @hasSection('content')
+            @yield('content')
+        @endif
+    </main>
+    @hasSection('javascript')
+        @yield('javascript')
+    @endif
+</body>
+</html>
