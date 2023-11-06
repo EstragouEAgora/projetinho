4ded9ad7248895b7444d897217966f93366f10d4
EstragouEAgora <projetoestragoueagora@gmail.com>
2023-11-06 10:45:55 -0300

    alteração

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
-
+composer install
 
 echo "Caching config..."
 php artisan config:cache
